7a2590c53766da2a9b852b79b60790303ff92288
wyt0220 <137224422+wyt0220@users.noreply.github.com>
2023-07-11 14:39:33 -0400

    Senti test_22_1112.py

--- Senti test_22_1112.py
+++ Senti test_22_1112.py
@@ -1,9 +1,7 @@
 import os
 
-# 桌面文件夹路径
 desktop_path = r"C:\Users\19599\OneDrive"
 
-# 递归函数来读取txt文件并提取内容
 def extract_content_between_fields(folder_path, field1, field2):
     for root, dirs, files in os.walk(folder_path):
         for file in files:
@@ -11,14 +9,11 @@ def extract_content_between_fields(folder_path, field1, field2):
                 file_path = os.path.join(root, file)
                 with open(file_path, 'r', encoding="utf-8") as txt_file:
                     file_contents = txt_file.read()
-                    # 提取item2和item3之间的内容
                     start_index = file_contents.find(field1) + len(field1)
                     end_index = file_contents.find(field2)
                     extracted_content = file_contents[start_index:end_index].strip()
-                    # 处理提取的内容
                     print(extracted_content)
 
-# 调用函数来提取桌面文件夹中每个txt文件的内容
 extract_content_between_fields(desktop_path, "item2", "item3")
 
 from bs4 import BeautifulSoup
