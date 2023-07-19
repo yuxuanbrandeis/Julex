d5ed8c4b33898c951ccdd227b7652c28630d850c
hasanallahyarov1 <126899036+hasanallahyarov1@users.noreply.github.com>
2023-07-18 23:33:06 -0400

    Update Individual_file_extraction.py

--- main_folder/Individual_file_extraction.py
+++ main_folder/Individual_file_extraction.py
@@ -114,4 +114,4 @@ def extract(file_name):
     clean_string = re.sub(r"\bquantitative\b", "", clean_string, flags=re.IGNORECASE).strip()
     
     clean_string= "Management's " + clean_string
-    print(clean_string)
+    return clean_string
