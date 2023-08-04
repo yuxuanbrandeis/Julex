538e4a2f06d15accf65482bfb960feb4eba6fdf2
hasanallahyarov1 <126899036+hasanallahyarov1@users.noreply.github.com>
2023-08-04 16:15:43 -0400

    Delete stock_price_example.py

--- main_folder/stock_price_example.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yahooFinance
-import pandas as pd
-
-GetFacebookInformation = yahooFinance.Ticker("META")
-
-pd.set_option('display.max_rows', None)
-
-META_price = GetFacebookInformation.history(period="max")
-print(META_price)
